1320fa9c879d694b5837c7260653054b10e70385
ksitrrss <61037383+ksitrrss@users.noreply.github.com>
2020-02-26 14:16:07 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,28 @@ import numpy as np
 import datetime, time
 import json
 import dlib
+mport cv2
 # start the camera
 cap = cv2.VideoCapture(args.src)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.w)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.h)
 ret, frame = cap.read()
+
+ #face detection and recognition
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+cap = cv2.VideoCapture(0)  
+while 1:  
+    ret, img = cap.read()   
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+  
+    for (x,y,w,h) in faces:  
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = img[y:y+h, x:x+w] 
+    cv2.imshow('img',img) 
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
+        break
+cap.release()  
+cv2.destroyAllWindows() 
