82f73c112325cde70e0f598c052aad4fa68775d9
ksitrrss <61037383+ksitrrss@users.noreply.github.com>
2020-02-26 13:36:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
 import cv2
 import numpy as np
 import datetime, time
+# start the camera
+cap = cv2.VideoCapture(args.src)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, args.w)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, args.h)
+ret, frame = cap.read()
